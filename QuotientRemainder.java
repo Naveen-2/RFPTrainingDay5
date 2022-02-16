85277d12672bfe9dacf1c3bd46c5f0f2904357c4
Naveen-2 <naveen2k15cse@gmail.com>
2022-02-16 22:09:48 +0530

    computed quotient and remainder

---
+++ QuotientRemainder.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class QuotientRemainder{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a Dividend: ");
+        int dividend = input.nextInt();
+        System.out.print("Enter a Divisor: ");
+        int divisor = input.nextInt();
+        
+        System.out.println("Quotient of "+dividend+" divided by 5: " + dividend/divisor);
+        System.out.println("Remainder of "+dividend+" divided by 5: " + dividend%divisor);
+        input.close();   
+    }
+}
